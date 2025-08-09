48b8dd0b40b996c64bdd21e959098762bfd42758
Raix <sebastianclopez@outlook.es>
2025-08-08 21:40:18 -0500

    Se subieron los cambios

---
+++ 03-control-flujo/07-for-of.js
@@ -0,0 +1,11 @@
+let animales = ['Chanchito feliz','Dragon','Perrito'];
+
+for(let animal of animales){
+    console.log(animales);
+}
+let i;
+//se usa .length para saber cuantos elementos hay en el array
+while(i<animales.length){
+    console.log(animales[i]);
+    i++;
+}
